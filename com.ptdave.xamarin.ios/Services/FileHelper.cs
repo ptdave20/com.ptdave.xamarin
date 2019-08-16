b414d694c358891d408523f27c139c20ee42b967
David Marchbanks <ptdave20@gmail.com>
2019-08-16 14:30:14 -0400

    Nuget FileHelper

---
+++ com.ptdave.xamarin.ios/Services/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using com.ptdave.xamarin.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(com.ptdave.xamarin.Services.IFileHelper))]
+namespace com.ptdave.xamarin.ios.Services
+{
+    public class FileHelper : IFileHelper
+    {
+        public string DatabaseFile(string filename)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "..", "Library", filename);
+        }
+    }
+}
---
+++ com.ptdave.xamarin/Services/IFileHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.ptdave.xamarin.Services
+{
+    public interface IFileHelper
+    {
+        string DatabaseFile(string filename);
+    }
+}
